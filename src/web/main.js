f6470ff8a81521bddf634b78a6e2159a4b86859f
Jeff Kowalski <jeff.kowalski@gmail.com>
2024-06-28 14:59:06 -0700

    clarify code in calculateDistance function

--- src/web/main.js
+++ src/web/main.js
@@ -215,16 +215,19 @@ function openTab(tabName)
 // ----------------------------------------------------------------------------
 
 // Function to calculate distance between two points using the Haversine formula
+// returns distance in kilometers
 function calculateDistance(lat1, lon1, lat2, lon2) {
-  const R = 6371; // Earth's radius in kilometers
-  const dLat = (lat2 - lat1) * (Math.PI / 180);
-  const dLon = (lon2 - lon1) * (Math.PI / 180);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c;
+    function toRad(x) { return x * Math.PI / 180; }
+    function squared (x) { return x * x }
+
+    const R = 6371; // Earth's radius in kilometers
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+    const a = squared(Math.sin(dLat / 2)) +
+          Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+          squared(Math.sin(dLon / 2));
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
 }
 
 async function getLocation() {
