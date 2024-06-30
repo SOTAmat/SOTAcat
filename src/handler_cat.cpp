de46299c3ac2c047f32a3717bf84d2a47bc33085
Jeff Kowalski <jeff.kowalski@gmail.com>
2024-06-29 20:09:15 -0700

    delay while keying cw message
    Wait for the radio to key the messsage, so we don't switch back to prior
    mode or send our webresponse to client too early.
    Implements a reasonable delay based on message length.  Further
    considerations entered in the code comments.

--- src/handler_cat.cpp
+++ src/handler_cat.cpp
@@ -7,7 +7,7 @@
 static const char * TAG8 = "sc:hdl_cat ";
 
 /**
- * Handles an HTTP PUT request to play a pre-recorded message
+ * Handles an HTTP PUT request to play a pre-recorded message from bank 1 or 2
  *
  * @param req Pointer to the HTTP request structure.  The "bank" query parameter
  *            is expected to hold either "1" or "2".
@@ -31,6 +31,14 @@ esp_err_t handler_msg_put (httpd_req_t * req) {
     REPLY_WITH_SUCCESS();
 }
 
+/**
+ * Handles an HTTP GET request to retrieve the current power level.
+ *
+ * This function processes the HTTP GET request and retrieves the current power level
+ * from the radio. The power level is then sent back as the HTTP response.
+ *
+ * @param req Pointer to the HTTP request structure.
+ */
 esp_err_t handler_power_get (httpd_req_t * req) {
     showActivity();
 
@@ -50,6 +58,16 @@ esp_err_t handler_power_get (httpd_req_t * req) {
     return ESP_OK;
 }
 
+/**
+ * Handles an HTTP PUT request to set the power level.
+ *
+ * This function processes the HTTP PUT request by decoding the "power" query parameter,
+ * which specifies the new power level to set. It sends a command to the radio to update
+ * the power level accordingly.
+ *
+ * @param req Pointer to the HTTP request structure. The "power" query parameter
+ *            is expected to hold a string representing the new power level.
+ */
 esp_err_t handler_power_put (httpd_req_t * req) {
     showActivity();
 
@@ -67,6 +85,18 @@ esp_err_t handler_power_put (httpd_req_t * req) {
     REPLY_WITH_SUCCESS();
 }
 
+/**
+ * Handles an HTTP PUT request to send a Morse code message.
+ *
+ * This function processes the HTTP PUT request by decoding the "message" query
+ * parameter, which specifies the Morse code message to be sent. The message is
+ * URL-decoded, and then a command is constructed to send the message via the
+ * radio. The function also manages the radio mode and calculates the necessary
+ * delay before restoring the mode based on the message length and speed.
+ *
+ * @param req Pointer to the HTTP request structure. The "message" query parameter
+ *            is expected to hold the text to be transmitted in Morse code.
+ */
 esp_err_t handler_keyer_put (httpd_req_t * req) {
     showActivity();
 
@@ -81,23 +111,32 @@ esp_err_t handler_keyer_put (httpd_req_t * req) {
     snprintf (command, sizeof (command), "KYW%s;", param_value);
     {
         const std::lock_guard<Lockable> lock (kxRadio);
-        radio_mode_t                    mode;
 
-        mode = (radio_mode_t)kxRadio.get_from_kx ("MD", SC_KX_COMMUNICATION_RETRIES, 1);
+        radio_mode_t mode            = (radio_mode_t)kxRadio.get_from_kx ("MD", SC_KX_COMMUNICATION_RETRIES, 1);
+        long         speed_wpm       = kxRadio.get_from_kx ("KS", SC_KX_COMMUNICATION_RETRIES, 3);
+        long         chars_remaining = strlen (param_value);
+
         if (mode != MODE_CW)
             kxRadio.put_to_kx ("MD", 1, MODE_CW, SC_KX_COMMUNICATION_RETRIES);
         kxRadio.put_to_kx_command_string (command, 1);
+
         /**
-         * TODO: wait for keying to complete before changing mode back This can
-         * be a combination of a smart delay computed by taking WPM, message
-         * size, and standard dot length into account As well, it can finish by
-         * querying the radio for characters remaining to transmit, and
-         * repeating that polling using shorter cycle informed by the queue
-         * length. See "TBX" command. As it stands, the mode switch will be
-         * queued, and won't disrupt transmission, but since the command returns
-         * immediately and says we're still in CW mode, it looks like a failure
-         * in the logs.
+         * NOTE: Ideally, we'd have a do-while loop here looking at the
+         * remaining queue of characters to be transmitted, and looping to delay
+         * further if there are any. Regrettably, the response format of the
+         * relevant "TBX;" command is of variable length since it shows the
+         * count of characters (good) and also what they are (bad). The
+         * combination is bad for us, because we currently don't have an
+         * accommodation in the uart communications for variable-length
+         * responses. But we're conservative enough in this current
+         * implementation that we come quite close to being contemporaneous with
+         * the conclusion of the transmission.
          */
+        long duration_ms = 60 * 1000 * chars_remaining / (speed_wpm * 5);
+        ESP_LOGI (TAG8, "delaying %ld ms for %ld chars at %ld wpm", duration_ms, chars_remaining, speed_wpm);
+        vTaskDelay (pdMS_TO_TICKS (duration_ms));
+
+        vTaskDelay (pdMS_TO_TICKS (600));  // an additional amount, once, for command processing
         if (mode != MODE_CW)
             kxRadio.put_to_kx ("MD", 1, mode, SC_KX_COMMUNICATION_RETRIES);
     }
