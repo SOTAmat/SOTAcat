31c9e29dbaefc56431733784d0994b2c6b9b5908
Jeff Kowalski <jmk999999@yahoo.com>
2025-05-29 10:37:07 -0700

    improve safety of Lockable class
    disable all copying, moving, and implicit construction
    provide destructor
    log error on failure to create mutex

--- src/lockable.cpp
+++ src/lockable.cpp
@@ -1,6 +1,6 @@
 #include "lockable.h"
-
 #include <esp_log.h>
+
 static const char * TAG8 = "sc:lockable";
 
 /**
@@ -14,10 +14,23 @@ Lockable::Lockable (char const * name)
     : m_locked (false)
     , m_name (name) {
     m_mutex = xSemaphoreCreateMutex();
+    if (!m_mutex) {
+        ESP_LOGE (TAG8, "Failed to create mutex for %s", m_name);
+        abort();  // or handle more gracefully
+    }
+}
+
+Lockable::~Lockable() {
+    if (m_mutex) {
+        vSemaphoreDelete (m_mutex);
+        m_mutex = nullptr;
+    }
 }
 
 void Lockable::lock() {
     ESP_LOGD (TAG8, "locking %s", m_name);
+    if (m_locked)
+        ESP_LOGE (TAG8, "double-lock detected!");
     xSemaphoreTake (m_mutex, portMAX_DELAY);
     m_locked = true;
     ESP_LOGD (TAG8, "%s LOCKED --", m_name);
